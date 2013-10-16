660fcc61c64fcccb51d33277f608f52d196d5acd
Fernando <fernando.amdrade@gmail.com>
2013-10-16 14:01:13 -0300

    setInterval

--- js/mapa.js
+++ js/mapa.js
@@ -2,7 +2,7 @@ var map;
 var idInfoBoxAberto;
 var infoBox = [];
 var markers = [];
-var coords = [];
+var coords = new Array();
 
 function initialize() {	
 	var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
@@ -30,57 +30,49 @@ function abrirInfoBox(id, marker) {
 function carregarCoordenadas(){
 	$.getJSON('js/pontos.json', function(pontos) {
 		$.each(pontos, function(index, ponto) {
-			coords.push(new google.maps.LatLng(ponto.Latitude, ponto.Longitude));
+			coords.push({p:new google.maps.LatLng(ponto.Latitude, ponto.Longitude),'Id':ponto.Id,'Descricao':ponto.Descricao});
 		});
 	});
 }
 
-
-function carregarPontos() {
-	 
-	$.getJSON('js/pontos.json', function(pontos) {
-		
-		var latlngbounds = new google.maps.LatLngBounds();
-		var i = 0;
-		$.each(pontos, function(index, ponto) {
-			
-			var interval = setInterval(function() {
-				var marker = new google.maps.Marker({
-					position: new google.maps.LatLng(ponto.Latitude, ponto.Longitude),
-					title: "Meu ponto personalizado! :-D",
-					icon: 'img/marcador.png',
-					animation: google.maps.Animation.DROP,
-					map: map
-				});	
-
-				var myOptions = {
-					content: "<p>" + ponto.Descricao + "</p>",
-					pixelOffset: new google.maps.Size(-150, 0)
-        		};
-
-				infoBox[ponto.Id] = new InfoBox(myOptions);
-				infoBox[ponto.Id].marker = marker;
-			
-				infoBox[ponto.Id].listener = google.maps.event.addListener(marker, 'mouseover', function (e) {
-					abrirInfoBox(ponto.Id, marker);
-				});
-			
-				markers.push(marker);
-			
-				latlngbounds.extend(marker.position);
-				console.log(pontos.length);
-				i++;
-				if(i >= pontos.length)
-					clearInterval(interval);
-			},1000);
+carregarCoordenadas();
+function marcarPontos(){
+	var latlngbounds = new google.maps.LatLngBounds();
+	var i = 0;
+	console.log(coords[i]);
+	var interval = setInterval(function(){
+		var marker = new google.maps.Marker({
+			map: map,
+			animation: google.maps.Animation.DROP,
+			position: coords[i].p
 		});
 		
-		var markerCluster = new MarkerClusterer(map, markers);
+		var myOptions = {
+					content: "<p>" + coords[i].Descricao + "</p>",
+					pixelOffset: new google.maps.Size(-150, 0)
+        		};
+var id = coords[i].Id;
+		infoBox[coords[i].Id] = new InfoBox(myOptions);
+		infoBox[coords[i].Id].marker = marker;
+			
+		infoBox[coords[i].Id].listener = google.maps.event.addListener(marker, 'mouseover', function (e) {
+			abrirInfoBox(id, marker);
+		});
 		
-		map.fitBounds(latlngbounds);		
-		
-	});
+		markers.push(marker);
+			
+		latlngbounds.extend(marker.position);
+
+		i++;
+		if(i>=coords.length){
+			clearInterval(interval);
+			var markerCluster = new MarkerClusterer(map, markers);
+			map.fitBounds(latlngbounds);
+		}
+			
+	},1000);
 	
 }
 
-carregarPontos();
+
+//carregarPontos();
